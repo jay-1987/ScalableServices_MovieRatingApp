2dd52c144be1342e501fac85c8f4277c729d4c80
jay-1987 <38540472+jay-1987@users.noreply.github.com>
2022-04-22 18:05:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import pandas as pd
 #import urllib.request
 #import ssl
 #from flask_mysqldb import MySQL
@@ -19,7 +20,18 @@ mydb = mysql.connector.connect(
           #auth_plugin='mysql_native_password',
           port='25060')       
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/')
+def hello():
+    print("Welcome to movie rating app guys")
+    return 'Hello you can change route: /movies for entering rating or /rating to view ratings or /table to see moviedata'
+
+@app.route('/table')
+def table():
+    data = pd.read_csv('Movie_rating.csv')
+    return render_template('moviedata.html', tables=[data.to_html()], titles=[''])
+  
+
+@app.route("/movies", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         
@@ -38,6 +50,13 @@ def index():
         cur.close
         return 'success'
     return render_template('index.html')
+
+@app.route("/rating")
+def rating():  
+        cur = mydb.cursor(buffered=True)
+        cur.execute("select * from members")
+        ratingdetails = cur.fetchall()
+        return render_template('rating.html',ratingdetails=ratingdetails)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5000 ,debug=True)
